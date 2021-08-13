e14b269412fd473c654d8fd0a5f77ba338b22bc5
Andrey.Metelyov <andrey.metelyov@gmail.com>
2021-08-13 13:21:48 +0900

    Stage 6/6: Brush up your code

--- Coffee Machine/task/src/machine/CoffeeMachine.kt
+++ Coffee Machine/task/src/machine/CoffeeMachine.kt
@@ -1,16 +1,46 @@
 package machine
 
-class CoffeeMachine(var water: Int = 400, var milk: Int = 540, var beans: Int = 120, var cups: Int = 9, var money: Int = 550) {
+enum class State {
+    ACTION,
+    VARIANT,
+    FILL_WATER,
+    FILL_MILK,
+    FILL_BEANS,
+    FILL_CUPS
+}
+
+class CoffeeMachine(
+    var water: Int = 400,
+    var milk: Int = 540,
+    var beans: Int = 120,
+    var cups: Int = 9,
+    var money: Int = 550
+) {
+    private var state: State = State.ACTION
+
     fun printState() {
-        println("""
+        println(
+            """
             The coffee machine has:
             $water of water
             $milk of milk
             $beans of coffee beans
             $cups of disposable cups
             $money of money
-        """.trimIndent())
+        """.trimIndent()
+        )
         println()
+    }
+
+    fun getPrompt(): String {
+        return when (state) {
+            State.VARIANT -> "What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: "
+            State.ACTION -> "Write action (buy, fill, take, remaining, exit): "
+            State.FILL_WATER -> "Write how many ml of water do you want to add: "
+            State.FILL_MILK -> "Write how many ml of milk do you want to add: "
+            State.FILL_BEANS -> "Write how many grams of coffee do you want to add: "
+            State.FILL_CUPS -> "Write how many disposable cups of coffee do you want to add: "
+        }
     }
 
     private fun trackResources(
@@ -40,11 +70,11 @@ class CoffeeMachine(var water: Int = 400, var milk: Int = 540, var beans: Int = 
     }
 
     fun getCappuccino() {
-    // for the cappuccino, the coffee machine needs
-    // 200 ml of water,
-    // 100 ml of milk, and
-    // 12 g of coffee.
-    // It costs $6
+        // for the cappuccino, the coffee machine needs
+        // 200 ml of water,
+        // 100 ml of milk, and
+        // 12 g of coffee.
+        // It costs $6
         if (!trackResources(200, 100, 12, 1)) {
             return
         }
@@ -65,10 +95,10 @@ class CoffeeMachine(var water: Int = 400, var milk: Int = 540, var beans: Int = 
     }
 
     fun getEspresso() {
-    // For the espresso, the coffee machine needs
-    // 250 ml of water and
-    // 16 g of coffee beans.
-    // It costs $4.
+        // For the espresso, the coffee machine needs
+        // 250 ml of water and
+        // 16 g of coffee beans.
+        // It costs $4.
         if (!trackResources(water = 250, beans = 16, cups = 1)) {
             return
         }
@@ -80,12 +110,12 @@ class CoffeeMachine(var water: Int = 400, var milk: Int = 540, var beans: Int = 
     }
 
     fun getLatte() {
-    // For the latte, the coffee machine needs
-    // 350 ml of water,
-    // 75 ml of milk, and
-    // 20 g of coffee beans.
-    // It costs $7.
-        if (!trackResources(250, 75 , 16, 1)) {
+        // For the latte, the coffee machine needs
+        // 350 ml of water,
+        // 75 ml of milk, and
+        // 20 g of coffee beans.
+        // It costs $7.
+        if (!trackResources(250, 75, 16, 1)) {
             return
         }
         water -= 350
@@ -99,6 +129,68 @@ class CoffeeMachine(var water: Int = 400, var milk: Int = 540, var beans: Int = 
     fun takeMoney() {
         println("I gave you \$$money\n")
         money = 0
-//        printState()
+    }
+
+    fun process(command: String): Boolean {
+        return when (state) {
+            State.ACTION -> processAction(command)
+            State.VARIANT -> buyCoffe(command)
+            State.FILL_WATER -> fillWater(command)
+            State.FILL_MILK -> fillMilk(command)
+            State.FILL_BEANS -> fillBeans(command)
+            State.FILL_CUPS -> fillCups(command)
+        }
+        return true
+    }
+
+    private fun fillCups(command: String): Boolean {
+        val amount = command.toInt()
+        cups += amount
+        state = State.ACTION
+        return true
+
+    }
+
+    private fun fillBeans(command: String): Boolean {
+        val amount = command.toInt()
+        beans += amount
+        state = State.FILL_CUPS
+        return true
+    }
+
+    private fun fillMilk(command: String): Boolean {
+        val amount = command.toInt()
+        milk += amount
+        state = State.FILL_BEANS
+        return true
+    }
+
+    private fun fillWater(command: String): Boolean {
+        val amount = command.toInt()
+        water += amount
+        state = State.FILL_MILK
+        return true
+    }
+
+    private fun processAction(command: String): Boolean {
+        when (command) {
+            "buy" -> state = State.VARIANT
+            "fill" -> state = State.FILL_WATER
+            "take" -> takeMoney()
+            "remaining" -> printState()
+            "exit" -> return false
+        }
+        return true
+    }
+
+    private fun buyCoffe(command: String): Boolean {
+        when (command) {
+            "1" -> getEspresso()
+            "2" -> getLatte()
+            "3" -> getCappuccino()
+            "back" -> state = State.ACTION
+        }
+        state = State.ACTION
+        return true
     }
 }
